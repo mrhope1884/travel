052e31bc3ca1af31dc0ce9b613464028e11ab4ef
mrhope1884 <vuvietquan1884@gmail.com>
2026-09-19 04:35:00 +0700

    fix: cập nhật chính xác link database thật

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 module.exports.connect = async () => {
   try {
     // Thêm option serverSelectionTimeoutMS để Mongoose kiên nhẫn đợi kết nối hơn
-    const databaseUrl = "mongodb+srv://<username>:vietquan1884@cluster0.xxxx.mongodb.net/ten_database?retryWrites=true&w=majority";
+    const databaseUrl = "mongodb+srv://vuvietquan1884_db_user:vietquan1884@cluster0.v0euzvx.mongodb.net/travel?retryWrites=true&w=majority&appName=Cluster0";
 
     await mongoose.connect(databaseUrl, {
       serverSelectionTimeoutMS: 30000 
